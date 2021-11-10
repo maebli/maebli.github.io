3e18ff942f0b84449182adea172a7dfb11543eec
heroichornet <1138612+maebli@users.noreply.github.com>
2021-11-10 22:25:20 +0100

    fixing latest post

---
+++ assets/js/wasm_encrypt.d.ts
@@ -0,0 +1,34 @@
+/* tslint:disable */
+/* eslint-disable */
+/**
+* @param {string} plaintext
+* @returns {string}
+*/
+export function encrypt(plaintext: string): string;
+/**
+* @param {string} encrypted
+* @returns {string}
+*/
+export function decrypt(encrypted: string): string;
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly encrypt: (a: number, b: number, c: number) => void;
+  readonly decrypt: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_add_to_stack_pointer: (a: number) => number;
+  readonly __wbindgen_malloc: (a: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number) => number;
+  readonly __wbindgen_free: (a: number, b: number) => void;
+}
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {InitInput | Promise<InitInput>} module_or_path
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function init (module_or_path?: InitInput | Promise<InitInput>): Promise<InitOutput>;
